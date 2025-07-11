b331e09bf00d3697f32eae1f48b967b3e083695f
Tobias Wälde <tobias.waelde@gmail.com>
2025-07-11 15:20:58 +0200

    update env

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,19 +4,22 @@ import { cleanEnv, num, str } from 'envalid';
 import { Logger } from '@nestjs/common';
 
 const envPath = path.resolve(process.cwd(), '.env');
-dotenv.config({ path: envPath });
+dotenv.config({ path: envPath, quiet: true });
 
 /** App Environment */
 export const ENV = cleanEnv(process.env, {
   PORT: num({ default: 3001 }),
-
-  // DB
+  // db
   DATABASE_URL: str(),
   SHADOW_DATABASE_URL: str(),
-
+  // auth
+  API_TOKEN: str({
+    default: null,
+    desc: 'API Token to protect the API from unauthorized access.',
+  }),
   DATA_KEEP_SECONDS: num({
     default: 60 * 60 * 24,
-    desc: 'Nomber of seconds to keep created data in DB.',
+    desc: 'Number of seconds to keep created data in DB.',
   }), // 1 day
   PUPPETEER_TIMEOUT: num({
     default: 60 * 1000,
